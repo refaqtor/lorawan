2cd9645eb1a83ed7cb34a8f96bba152fb44377e9
Nikita Eliseev <eliseev@myriad-corp.com>
2017-07-25 18:49:10 +0300

    add docs and fix travis

--- source/lorawan/parser/packets.d
+++ source/lorawan/parser/packets.d
@@ -1,7 +1,11 @@
 module lorawan.parser.packets;
 
+/// Structure corresponding to the PUSH_ACK package
 struct PUSH_ACK{
+  /// Protocol version between Lora gateway and server
   ubyte protocolVersion = 2;
+  /// Same token as the PUSH_DATA packet to acknowledge
   ubyte[2] token;
+  ///PUSH_ACK identifier 0x01
   ubyte pushAckID = 1;
 }
--- source/lorawan/parser/parser.d
+++ source/lorawan/parser/parser.d
@@ -2,7 +2,9 @@ module lorawan.parser.parser;
 
 import lorawan.parser.packets;
 
+/// A class that provides parsing of structures corresponding to different types of packets, and parsing the input byte array into a structure of the required type
 class Parser{
+  /// Parsing a structure into an array of bytes
   ubyte[] parseStructure(PUSH_ACK pushAckStruct){
     ubyte[] pushAckArray = pushAckStruct.protocolVersion ~ pushAckStruct.token ~ pushAckStruct.pushAckID;
     return pushAckArray;
